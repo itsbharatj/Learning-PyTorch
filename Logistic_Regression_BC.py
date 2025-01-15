f82cf5e937da1d679eee356a7f4bd56599eb25dd
Bharat Jain <bharat.jain@plaksha.edu.in>
2025-01-15 22:17:55 +0530

    boiler plate Logistic Regression

---
+++ Logistic_Regression_BC.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn 
+import sklearn 
+import numpy as np
+
+
+'''
+Implement Logistic Regression of the BC Dataset
+
+Steps involved: 
+- Dowload the Dataset X and y 
+- Make sure to have the target 
+'''
