f16ff4d769335ea83a3e09956767d777db98b154
Bharat Jain <bharat.jain@plaksha.edu.in>
2025-01-19 15:45:45 +0530

    removed spaces from folder names

---
+++ Day_10_Dataset_Transformation/tranformations.py
@@ -0,0 +1,21 @@
+'''
+We have a PyTorch Dataset, which we need to get some transformations applied. 
+
+For this: 
+
+1. Our Dataset Class: 
+    - Add the transforms feature
+
+2. Make the tranform classes: 
+    - For Multiplication 
+    - For Addition
+
+3. Apply these tranformations and check it with the output that we have recieved
+
+4. Apply a combined tranformation using the composed method of torchvision
+
+5. Check that as well with the output that we have recieved 
+'''
+
+import torch 
+from Day 09 - Datasets and Dataloaders/dataloaders_datastes.py
