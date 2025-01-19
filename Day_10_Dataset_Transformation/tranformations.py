aba4833d10f1cd863b6f2b81dcdc5ea67e3b3ac6
Bharat Jain <bharat.jain@plaksha.edu.in>
2025-01-19 16:40:55 +0530

    added dataset tranforms + supporting files

--- Day_10_Dataset_Transformation/tranformations.py
+++ Day_10_Dataset_Transformation/tranformations.py
@@ -17,5 +17,56 @@ For this:
 5. Check that as well with the output that we have recieved 
 '''
 
+import sys
+import os
 import torch 
-from Day 09 - Datasets and Dataloaders/dataloaders_datastes.py
+import torchvision
+
+# from Day_09_Datasets_and_Dataloaders import dataloaders_datastes
+from dataloaders_datastes import Wine_Dataset
+
+class Tranform_Wine_Dataset(Wine_Dataset): 
+    def __init__(self,transforms=None,*args,**kwargs): 
+        super(Tranform_Wine_Dataset,self).__init__(*args,**kwargs)
+
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        sample =  super().__getitem__(index)
+
+        if self.transforms: 
+            return self.transforms(sample)
+        
+        return sample
+
+class Mul_Tranform: 
+    def __init__(self,factor=2): 
+        self.factor = factor
+    
+    def __call__(self,sample):
+        inputs,targets = sample
+        inputs *= self.factor
+        return inputs,targets
+
+class Add_Tranform: 
+    def __init__(self,term=10): 
+        self.add_term = term
+    def __call__(self,sample):
+        input, target = sample
+        input += self.add_term
+        return input, target 
+
+if __name__ == "__main__": 
+
+    dataset_normal = Tranform_Wine_Dataset()
+    dataset_transform = Tranform_Wine_Dataset(transforms=Add_Tranform(term=100))
+    
+    #This works! The individual transforms: 
+    # print(f'{dataset_normal[0]} \n {dataset_transform[0]}')
+
+    ## Trying the compose feature to combine multiple tranforms: 
+
+    compose = torchvision.transforms.Compose([Add_Tranform(term=100),Mul_Tranform()])
+    dataset_compose = Tranform_Wine_Dataset(transforms=compose)
+
+    print(f'Without Tranforms: {dataset_normal[10][0]} \n After Transforms: {dataset_compose[10][0]}')
