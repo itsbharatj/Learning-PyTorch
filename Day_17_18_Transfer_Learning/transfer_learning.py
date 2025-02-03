48f79ab6d65c843a2bb2f7074c146288d86f1f36
Bharat Jain <bharat.jain@plaksha.edu.in>
2025-02-03 13:26:41 +0530

    completed_dataloaders_datasets

--- Day_17_18_Transfer_Learning/transfer_learning.py
+++ Day_17_18_Transfer_Learning/transfer_learning.py
@@ -45,4 +45,38 @@ Elements:
 import torch 
 import torchvision 
 from torch.utils.data import DataLoader, Dataset
-from torchvision.transforms import transforms
+from torchvision import transforms 
+
+data_dir = "data/cats_dogs"
+sets = ["train","val"]
+n_vals = (0.5*3)
+batch_size = 4 
+lr = 0.001
+
+
+transforms_train = transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomCrop(225), 
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(), 
+        transforms.Normalize(mean=n_vals,std=n_vals)
+])
+
+transforms_val = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(225), 
+        transforms.ToTensor(), 
+        transforms.Normalize(mean=n_vals,std=n_vals)
+])
+
+train_data = torchvision.datasets.ImageFolder(root=data_dir,
+                    transform=transforms_train)
+val_data = torchvision.datasets.ImageFolder(root=data_dir,transform=transforms_val)
+
+## Dataloaders: 
+train_loader = DataLoader(dataset=train_data,batch_size=batch_size,lr=lr)
+val_loader = DataLoader(dataset=val_data,batch_size=batch_size,lr=lr)
+
+## Will have to read this over again: 
+def train():
+
