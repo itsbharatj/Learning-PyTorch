263a2f8463983ff712c79780d5b65a57118e7034
Bharat Jain <bharat.jain@plaksha.edu.in>
2025-02-03 12:42:27 +0530

    modified download dataset

--- Day_17_18_Transfer_Learning/data_download_kaggle.py
+++ Day_17_18_Transfer_Learning/data_download_kaggle.py
@@ -1,8 +1,13 @@
 import kagglehub
+import shutil
+import os
 
 # Download latest version
 dataset_handle_kaggle = "aleemaparakatta/cats-and-dogs-mini-dataset"
-save_path = "../data/"
+save_path = "data/"
 path = kagglehub.dataset_download("aleemaparakatta/cats-and-dogs-mini-dataset")
 
 print("Path to dataset files:", path)
+
+shutil.move(path,save_path)
+print("Data moved to data dir")
--- Day_17_18_Transfer_Learning/data_split.py
+++ Day_17_18_Transfer_Learning/data_split.py
@@ -1,9 +1,30 @@
 from torch.utils.data import random_split
 import os 
+import shutil
 
-data_dir = '~/Desktop/pytorch_course/data/cats_dogs'
-sets = ["train","val"]
+'''
+Data was downloaded in the form of: 
+/data: 
+    /class_1 
+    /class_2 
+
+Require: 
+/data 
+    /train
+        /class_1
+        /class_2
+    /val
+        /class_1 
+        /class_2
+
+Make the copies, copy the required data into the respective folders in the ratio
+'''
+
+data_dir = 'data/cats_dogs'
+sets = ["train"]
 folders = ["cats","dogs"]
+cats_dir = f'{data_dir}/{folders[0]}'
+dogs_dir = f'{data_dir}/{folders[1]}'
 
 for i in sets: 
     if not os.path.exists(f'{data_dir}/{i}'): 
@@ -15,5 +36,26 @@ files = []
 for i in folders:
     files.append({i:os.listdir(f'{data_dir}/{i}')})
  
-train_data = files["cats"]
-val_data = 
+ratio = 80
+cats_train = int(len(os.listdir(cats_dir))*(ratio/100))
+dogs_train = int(len(os.listdir(dogs_dir))*(ratio/100))
+
+train_cats_files = os.listdir(cats_dir)[:cats_train]
+train_dogs_files = os.listdir(dogs_dir)[:dogs_train]
+
+val_cats_files = os.listdir(cats_dir)[cats_train:]
+val_dogs_files = os.listdir(dogs_dir)[dogs_train:]
+
+## Move the files: 
+
+all_folders = [train_cats_files, train_dogs_files]
+all_dest_folders = [f'{data_dir}/train/cats', f'{data_dir}/train/dogs']
+
+for i, folder in enumerate(all_folders): 
+    for file in folder: 
+        shutil.move(f'{data_dir}/{folders[i]}/{file}', all_dest_folders[i])
+
+if not os.path.exists(f'{data_dir}/val'): 
+    os.mkdir(f'{data_dir}/val')
+    for i in folders: 
+        shutil.move(src=f'{data_dir}/{i}',dst=f'{data_dir}/{i}/val')
